898a4cd920edb59e7ca9d9b15925a0953a2e981d
gyureal <yonggyu.jeong10@gmail.com>
2023-09-30 09:58:05 +0900

    feat: youtube 동영상 재생 사이트 by react

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -10,13 +10,18 @@ class SearchBar extends Component {
 
     render() {
         return (
-            <div>
+            <div className='search-bar'>
                 <input 
                 value= {this.state.term} // state 값으로 대입
-                onChange = {event => this.setState({ term : event.target.value })}
+                onChange = {event => this.onInputChange(event.target.value)}
                 /> 
             </div>
         )  
+    }
+
+    onInputChange(term) {
+        this.setState({term});
+        this.props.onSearchTermChange(term);
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,53 @@
-import React from 'react';
+import _ from 'lodash';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail'
 
 const API_KEY = '';
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+YTSearch({key: API_KEY, term:'surfboard'}, function(data) {
+    console.log(data);
+});
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { 
+            videos : [],
+            selectedVideo : null
+        };
+
+        this.videoSearch('surfboard');
+    }
+
+    videoSearch(term) {
+        YTSearch({key: API_KEY, term:term}, (videos) => {
+            this.setState({ 
+                videos : videos,
+                selectedVideo: videos[0]
+            });
+            // this.setState({ videos });
+            // this.setState({ videos : videos });
+        });
+    }
+
+    render() {
+        const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 1000); // 1000ms의 딜레이가 있음
+
+        return (
+            <div>
+                 <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
+                 <VideoDetail video={this.state.selectedVideo} />
+                 <VideoList 
+                 onVideoSelect={(selectedVideo) => this.setState({selectedVideo})}
+                 videos={this.state.videos} />
+            </div>
+        );
+    }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
